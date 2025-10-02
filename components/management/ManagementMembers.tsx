d8193589c51a4434ad0a105f9caf911a0bf68252
dominicnaatey <dominicnaatey@gmail.com>
2025-10-02 16:27:22 +0000

    add new member and update existing member details

--- components/management/ManagementMembers.tsx
+++ components/management/ManagementMembers.tsx
@@ -50,12 +50,22 @@ const managementMembers: ManagementMember[] = [
       "Qualifying Certificate in Law - Ghana School of Law, Makola",
     ],
   },
-  {
+   {
     id: 3,
+    name: "Lariba Abaduri-Weseh",
+    position: "Head of Finance",
+    bio: "",
+    image: "/management/3.jpg",
+    qualifications: [
+      ""
+    ],
+  },
+  {
+    id: 4,
     name: "George Opoku-Ware Boateng",
     position: "Head of Legal",
     bio: "George Opoku-Ware Boateng holds a Master's Degree in Management and Administration from the University of Ghana Business School, Legon. He also earned a Bachelor of Arts in History and Religions and a Bachelor of Laws, both from the University of Ghana.<br><br>Called to the Ghana Bar in 2008, he has maintained an active membership with the Ghana Bar Association since then. Beginning his career at TDC as a Legal Officer in 2008, GEORGE OPOKU-WARE BOATENG has played an integral role in the company's legal endeavors, successfully prosecuting and defending major cases.<br><br>His extensive experience and steadfast commitment led to his appointment as Head of Legal in 2025, where he continues to provide expert legal oversight and guidance.",
-    image: "/management/3.jpg",
+    image: "/management/4.jpg",
     qualifications: [
       "Master's Degree in Management and Administration - University of Ghana Business School, Legon",
       "Bachelor of Laws - University of Ghana",
@@ -65,11 +75,11 @@ const managementMembers: ManagementMember[] = [
     ],
   },
   {
-    id: 4,
+    id: 5,
     name: "DOROTHY A. ASARE-KUMAH ADOLF, ESQ",
     position: "Board Secretary and Head of Corporate Planning & Communication",
     bio: "Mrs. Asare-Kumah Adolf is the Head of Corporate Planning and Communications at TDC Ghana Ltd. She holds dual Master's degrees in Communications and Law (LLM) from Grand Valley University, Michigan, USA, and the University of Law, UK, respectively. Additionally, she holds a Postgraduate Diploma in Education from the University of Education, Winneba, and a Diploma in Journalism from the Ghana Institute of Journalism.<br><br>Prior to joining TDC nearly two decades ago, Mrs. Asare-Kumah Adolf established her career in journalism and lectured briefly at Tarrant County Community College in Fort Worth, Texas, as well as at the Ghana Institute of Journalism. As a former workers' Union leader, she continuously utilised her dual legal and communications expertise in staff welfare advocacy and institutional reforms.<br><br>She is a member of the Ghana Bar Association, the Ghana Journalists Association and the Institute of Public Relations, Ghana.",
-    image: "/management/4.jpg",
+    image: "/management/5.jpg",
     qualifications: [
       "Master's Degree in Communications - Grand Valley University, Michigan, USA",
       "Master of Laws (LLM) - University of Law, UK",
@@ -81,11 +91,11 @@ const managementMembers: ManagementMember[] = [
     ],
   },
   {
-    id: 5,
+    id: 6,
     name: "John Felix Wardy",
     position: "Head of Administration",
     bio: "John Felix Wardy joined TDC in June 2006. He began his career in the judicial service of Ghana.In 1992 he joined the judicial service of Ghana as an Assistant Registrar.<br><br>In 1999, John was assigned the position of Circuit Court Registrar attached as a probationary position awaiting confirmation as High Court Registrar.<br><br>In 2000 he was promoted to the rank of High Court Registrar attached to the District Court.Here,he was in charge of the day-to-day runing of the court and the personnel attached.His schedule include preparation of affidavits,writs of summons,conducting marriage ceremonies,strategically enforcing the orders of the court such as writ of fifa,absconding warrants and Garnishee order.<br><br>In 2005, John as a high court Registrar was attached to High Court I,II,III and IV.Here,John served as deputy administrator for four different high courts.Quite apart from performing similar duties outlined he was also in charge of wills,settlement of appeal records and also doubled as a Commissioner for Oaths.<br><br>In 2006, he resigned from the Judicial Service to join TDC.<br><br>John holds a Bachelor of Arts (BA) degree from the University of Cape Coast and a Master of Public Administration (MPA) from the University of Ghana,Legon. John is also a member of the Institute of Human Resource Practitioners (Ghana).",
-    image: "/management/5.jpg",
+    image: "/management/6.jpg",
     qualifications: [
       "Bachelor of Arts (BA) - University of Cape Coast",
       "Master of Public Administration (MPA) - University of Ghana, Legon",
@@ -94,11 +104,11 @@ const managementMembers: ManagementMember[] = [
     ],
   },
   {
-    id: 6,
+    id: 7,
     name: "Samuel Abutiate",
     position: "Chief Internal Auditor",
     bio: "A Chartered Accountant by profession, Mr. Samuel Abutiate is a product of University of Ghana Business School, from where he holds a Bachelor of Science (BSc) Honors Degree in Accounting.<br><br>He also holds a Masters of Business Administration (MBA) Degree in Finance also from University of Ghana Business School.<br><br>He is a good standing member of the Institute of Chartered Accountants (ICA) Ghana, membership of Institute of Internal Auditors Ghana and obtained the Professional Level of the Chartered Institute of Taxation, Ghana.<br><br>He has over 22 years varied and extensive experience in the Services, Manufacturing and Real Estates Industries. Mr. Abutiate has detailed knowledge in Auditing, Accounting, Financial Management and Real Estate Development.<br><br>Samuel commenced his career at Ghana Post Company Limited in 2003 as Assistant Audit Manager. He moved to Crystal Auto Limited, a Manufacturing Company in 2007, as an Accounts Manager and later joined TDC Ghana Ltd in 2011, as an Internal Audit Manager.<br><br>Prior to his appointment as Ag. Chief Internal Auditor, he attended many capacity building and management trainee workshops. He served in various capacities and on various committees. He was an Executive Member of the Senior Staff Association in the Company for two consecutive terms.<br><br>In his current position, he manages and oversees the activities of the Internal Audit Department.",
-    image: "/management/6.jpg",
+    image: "/management/7.jpg",
     qualifications: [
       "Bachelor of Science (BSc) Honours in Accounting - University of Ghana Business School",
       "Master of Business Administration (MBA) in Finance - University of Ghana Business School",
@@ -108,11 +118,11 @@ const managementMembers: ManagementMember[] = [
     ],
   },
   {
-    id: 7,
+    id: 8,
     name: "Sarah Donkor",
     position: "Head of Estates",
     bio: "Surveyor Sarah Donkor (Mrs) has over two decades experience in Land Administration and Management.<br><br> She joined TDC in 1999 as a National Service person and has risen through the ranks to her current position as the Head of Estates.<br><br> She is a Chartered Valuer and a professional member of the Ghana Institution of Surveyors (GhIS). She holds a BSC in Land Economy and MSc in Real Estate from the Kwame Nkrumah University of Science and Technology. Kumasi. She has attended several Capacity Building courses in Valuation, Land Administration and Management. She serves on the Board of Regency Engineering and Construction Limited (REC), a Ghanaian company which offers Engineering and Project Management services.",
-    image: "/management/7.jpg",
+    image: "/management/8.jpg",
     qualifications: [
       "BSc in Land Economy - Kwame Nkrumah University of Science and Technology, Kumasi",
       "MSc in Real Estate - Kwame Nkrumah University of Science and Technology, Kumasi",
@@ -121,6 +131,7 @@ const managementMembers: ManagementMember[] = [
       "Board Member - Regency Engineering and Construction Limited (REC)",
     ],
   },
+  
 ];
 
 export default function ManagementMembers() {
