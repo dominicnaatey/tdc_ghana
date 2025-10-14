3155859901cae8bdf0ebfc8faabd1a8ef5f673c5
dominicnaatey <dominicnaatey@gmail.com>
2025-10-14 07:20:14 +0000

    adjust grid layout and spacing for PastManagingDirectors

--- components/about/PastManagingDirectors.tsx
+++ components/about/PastManagingDirectors.tsx
@@ -37,7 +37,7 @@ export default function PastManagingDirectors() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 lg:gap-6">
           {images.map((src, idx) => (
             <div
               key={src}
